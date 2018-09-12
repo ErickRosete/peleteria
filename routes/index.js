ee765cd539e34b549e7162bfe11c64b5580556d4
Oscar Rosete <oscarrosete@MacBook-Pro-de-Estefania.local>
2018-09-11 19:05:56 -0700

    cambio estructura

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,9 @@ router.get('/', function (req, res) {
     // res.statusCode = 200;
     // res.setHeader('Content-Type', 'text/plain');
     // res.end('Hello World\n');
-    res.render("landing") 
+    res.render("landing", {
+        my_title : "Inicio"
+    });
     // diapositiva2(res)
 });
 router.get('/products', function (req, res) {

3734aedce2d4e92c6918851d66e3e33b353366fb
Oscar Rosete <oscaralonso11@hotmail.com>
2019-06-04 12:53:36 -0700

    all is repaired

--- public/js/languages/languages.js
+++ public/js/languages/languages.js
@@ -12,10 +12,22 @@ $(function () {
 
     function setLang(lang) {
         $('.lang').each(function (index, element) {
-            $(this).text(arrLang[lang][$(this).attr('key')]);
+            $(element).text(arrLang[lang][$(element).attr('key')]);
         });
         $('.lang-placeholder').each(function (index, element) {
             $(this).attr("placeholder", arrLang[lang][$(this).attr('key')]);
         });
+        // let cambiables=document.querySelectorAll(".lang")
+        // cambiables.forEach((elemento)=>{
+        //     console.log(elemento.textContent)
+        //     // console.log(arrLang[lang])
+        //     // console.log((elemento).getAttribute('key'))
+        //     console.log(arrLang[lang][(elemento).getAttribute('key')])
+        //     elemento.textContent=arrLang[lang][(elemento).getAttribute('key')]
+        //     console.log(lang)
+        // })
+    //     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    //     integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    // </script>
     }
 });
